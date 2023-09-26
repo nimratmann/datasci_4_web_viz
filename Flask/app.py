b0da621ff4713553af983338e453120d138c1f63
nimratmann <nimratmann50@gmail.com>
2023-09-26 18:05:56 +0000

    Updated app.py

--- Flask/app.py
+++ Flask/app.py
@@ -14,11 +14,11 @@ app = Flask(__name__)
 # Load the dataset
 url = "https://raw.githubusercontent.com/nimratmann/datasci_4_web_viz/main/datasets/PLACES__Local_Data_for_Better_Health__County_Data_2023_release.csv"
 df = pd.read_csv(url)
-df_obesity = df[(df['MeasureId'] == 'OBESITY') & (df['Data_Value_Type'] == 'Crude prevalence')]
+df_arthritis = df[(df['MeasureId'] == 'ARTHRITIS') & (df['Data_Value_Type'] == 'Crude prevalence')]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    counties = sorted(df_obesity['LocationName'].unique())
+    counties = sorted(df_arthritis['LocationName'].unique())
     selected_county = request.form.get('county') or counties[0]
     
     img = create_plot(selected_county)
@@ -26,8 +26,8 @@ def index():
     return render_template("index.html", counties=counties, selected_county=selected_county, img=img)
 
 def create_plot(county):
-    overall_avg = df_obesity['Data_Value'].mean()
-    selected_county_avg = df_obesity[df_obesity['LocationName'] == county]['Data_Value'].mean()
+    overall_avg = df_arthritis['Data_Value'].mean()
+    selected_county_avg = df_arthritis[df_arthritis['LocationName'] == county]['Data_Value'].mean()
 
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.bar(['County (California)', 'Overall Average'], [selected_county_avg, overall_avg], color=['lightcoral', 'dodgerblue'])
